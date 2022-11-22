afe6b60dc384f8bd14a4d479e00742219e8ea423
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2022-11-22 11:32:09 +0100

    Add types to package index

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,2 +1,3 @@
 export { default as useFlow } from './useFlow';
-export { default as flowAsState } from './flowAsState';
+export { default as flowAsState, State } from './flowAsState';
+export { Next, Next1, Next2, NextX } from './useFlow.types';
