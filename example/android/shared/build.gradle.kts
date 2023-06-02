06060b5b967d6f1553e9b2999643b57261df578e
Erik Ziegler <erik@voize.de>
2023-06-02 21:43:21 +0200

    Try reverting default hierarchy in example project

--- example/android/shared/build.gradle.kts
+++ example/android/shared/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-
 plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
@@ -9,10 +7,7 @@ plugins {
 
 val reaktNativeToolkitVersion = "0.5.0"
 
-@OptIn(ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    targetHierarchy.default()
-
     android()
 
     listOf(
@@ -46,6 +41,7 @@ kotlin {
                 implementation("de.voize:reakt-native-toolkit:$reaktNativeToolkitVersion") {
                     exclude("com.facebook.react", "react-native")
                 }
+
             }
         }
         val commonTest by getting {
@@ -61,6 +57,8 @@ kotlin {
                 implementation("com.facebook.react:react-native:+")
             }
         }
+        val androidTest by getting
+
 
         val iosX64Main by getting {
             kotlin.srcDir("build/generated/ksp/iosX64/iosX64Main/kotlin")
@@ -70,6 +68,23 @@ kotlin {
         }
         val iosSimulatorArm64Main by getting {
             kotlin.srcDir("build/generated/ksp/iosSimulatorArm64/iosSimulatorArm64Main/kotlin")
+        }
+
+        val iosMain by creating {
+            dependsOn(commonMain)
+
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
