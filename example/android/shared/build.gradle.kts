b30e8ab9aaa91728df2261d8a640f9fd4aedd6f9
Leon Kiefer <leon.k97@gmx.de>
2025-10-16 00:09:35 +0200

    version 0.21.10

--- example/android/shared/build.gradle.kts
+++ example/android/shared/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.0.21-1.0.28"
 }
 
-val reaktNativeToolkitVersion = "0.21.9"
+val reaktNativeToolkitVersion = "0.21.10"
 
 kotlin {
     androidTarget {
